d90052d05c7aa4ed7f2c25d1ac0c4c19830575cf
MDjaved2834 <163591188+MDjaved2834@users.noreply.github.com>
2024-03-16 14:07:00 +0530

    print 123

--- non_primitve_data_type/Hello.java
+++ non_primitve_data_type/Hello.java
@@ -3,5 +3,6 @@ package non_primitve_data_type;
 public class Hello {
     public static void main(String[] args) {
         System.out.print("Hello World");
+        System.out.print("123");
     }
 }
